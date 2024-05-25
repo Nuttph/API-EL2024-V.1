f92c39e579b4d41c8c0ebc9cdfcaf63f42843d16
Nuttph <nathn2420@gmail.com>
2024-05-25 12:43:09 +0700

    add comit

--- client/src/components/Code/ShowData.tsx
+++ client/src/components/Code/ShowData.tsx
@@ -1,24 +1,11 @@
 import React, { useEffect, useState } from "react";
 
-const ShowData = () => {
-  const [data, setData] = useState([]);
+//data
+import dataJson from "@/components/Code/data.json";
 
-  useEffect(() => {
-    const dataFetching = async () => {
-      try {
-        console.log("Stats fetch");
-        const res = await fetch("http://localhost:4000/result");
-        const rawData = await res.json();
-        setData(rawData);
-        // console.log(rawData[0].name);
-      } catch (err) {
-        console.log("It's error because => ", err);
-      } finally {
-        console.log("Finish Fetching");
-      }
-    };
-    dataFetching();
-  }, []);
+const ShowData = () => {
+  const [data, setData] = useState(dataJson);
+
   return (
     <>
       <div>
